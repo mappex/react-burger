8621d7afca844c2317596df56739802e874dfb3d
Konstantin Kaduckiy <kkaduckiy@rvision.ru>
2023-03-31 18:48:43 +0000

    правки на ответ ревьювера

--- cypress/integration/burger-constructor.spec.js
+++ cypress/integration/burger-constructor.spec.js
@@ -1,4 +1,4 @@
-const { SELECTORS } = require('../support/constants');
+const { SELECTORS, TESTKEY } = require('../support/constants');
 
 describe('burger constructor functional tests', () => {
   beforeEach(() => {
@@ -6,33 +6,33 @@ describe('burger constructor functional tests', () => {
     cy.intercept('GET', `${Cypress.env('BASE_URL')}/ingredients`, {
       fixture: 'ingredients.json',
     });
-    cy.get('[data-test-id="burger-constructor"]').as('constructor');
-    cy.get('[data-test-id="burger-ingredient"]').as('ingredient');
-    cy.get('[data-test-id="total-wrapper"]').as('totalWrapper');
+    cy.get(`[${TESTKEY.DATA_TEST_ID}="burger-constructor"]`).as('constructor');
+    cy.get(`[${TESTKEY.DATA_TEST_ID}="burger-ingredient"]`).as('ingredient');
+    cy.get(`[${TESTKEY.DATA_TEST_ID}="total-wrapper"]`).as('totalWrapper');
   });
 
   it('has burger ingredients container', () => {
-    cy.get('[data-test-id="burger-ingredients"]').should('have.length', 1);
+    cy.get(`[${TESTKEY.DATA_TEST_ID}="burger-ingredients"]`).should('have.length', 1);
   });
 
   it('has burger ingredients', () => {
-    cy.get('@ingredient').should('have.length.above', 0);
+    cy.get(SELECTORS.INGREDIENT).should('have.length.above', 0);
   });
 
   it('has burger constructor', () => {
-    cy.get('@constructor').should('have.length', 1);
+    cy.get(SELECTORS.CONSTRUCTOR).should('have.length', 1);
   });
 
   it('can drag and drop a bun', () => {
-    cy.get('@ingredient').eq(0).as('first');
+    cy.get(SELECTORS.INGREDIENT).eq(0).as('first');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       0
     );
-    cy.get('@first').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@first').find('p').should('have.text', 2);
-    cy.get('@totalWrapper').should('contain.text', '2510');
+    cy.get(SELECTORS.FIRST).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.FIRST).find('p').should('have.text', 2);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2510');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       2
@@ -40,25 +40,25 @@ describe('burger constructor functional tests', () => {
   });
 
   it('can drag and drop another bun', () => {
-    cy.get('@ingredient').eq(0).as('first');
-    cy.get('@ingredient').eq(1).as('second');
+    cy.get(SELECTORS.INGREDIENT).eq(0).as('first');
+    cy.get(SELECTORS.INGREDIENT).eq(1).as('second');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       0
     );
-    cy.get('@first').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@first').find('p').should('have.text', 2);
-    cy.get('@totalWrapper').should('contain.text', '2510');
+    cy.get(SELECTORS.FIRST).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.FIRST).find('p').should('have.text', 2);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2510');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       2
     );
-    cy.get('@second').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@first').find('p').should('not.exist');
-    cy.get('@second').find('p').should('have.text', 2);
-    cy.get('@totalWrapper').should('contain.text', '1976');
+    cy.get(SELECTORS.SECOND).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.FIRST).find('p').should('not.exist');
+    cy.get(SELECTORS.SECOND).find('p').should('have.text', 2);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '1976');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       2
@@ -66,33 +66,33 @@ describe('burger constructor functional tests', () => {
   });
 
   it('can add more ingredients', () => {
-    cy.get('@ingredient').eq(0).as('first');
-    cy.get('@ingredient').eq(2).as('third');
-    cy.get('@ingredient').eq(8).as('ninth');
+    cy.get(SELECTORS.INGREDIENT).eq(0).as('first');
+    cy.get(SELECTORS.INGREDIENT).eq(2).as('third');
+    cy.get(SELECTORS.INGREDIENT).eq(8).as('ninth');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       0
     );
-    cy.get('@first').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@first').find('p').should('have.text', 2);
-    cy.get('@totalWrapper').should('contain.text', '2510');
+    cy.get(SELECTORS.FIRST).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.FIRST).find('p').should('have.text', 2);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2510');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       2
     );
-    cy.get('@third').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@third').find('p').should('have.text', 1);
-    cy.get('@totalWrapper').should('contain.text', '2600');
+    cy.get(SELECTORS.THIRD).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.THIRD).find('p').should('have.text', 1);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2600');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       3
     );
-    cy.get('@ninth').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@ninth').find('p').should('have.text', 1);
-    cy.get('@totalWrapper').should('contain.text', '5600');
+    cy.get(SELECTORS.NINTH).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.NINTH).find('p').should('have.text', 1);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '5600');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       4
@@ -100,24 +100,24 @@ describe('burger constructor functional tests', () => {
   });
 
   it('can remove ingredients', () => {
-    cy.get('@ingredient').eq(0).as('first');
-    cy.get('@ingredient').eq(2).as('third');
+    cy.get(SELECTORS.INGREDIENT).eq(0).as('first');
+    cy.get(SELECTORS.INGREDIENT).eq(2).as('third');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       0
     );
-    cy.get('@first').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@first').find('p').should('have.text', 2);
-    cy.get('@totalWrapper').should('contain.text', '2510');
+    cy.get(SELECTORS.FIRST).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.FIRST).find('p').should('have.text', 2);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2510');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       2
     );
-    cy.get('@third').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@third').find('p').should('have.text', 1);
-    cy.get('@totalWrapper').should('contain.text', '2600');
+    cy.get(SELECTORS.THIRD).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.THIRD).find('p').should('have.text', 1);
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2600');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       3
@@ -126,7 +126,7 @@ describe('burger constructor functional tests', () => {
       .eq(1)
       .find('.constructor-element__action')
       .click();
-    cy.get('@totalWrapper').should('contain.text', '2510');
+    cy.get(SELECTORS.TOTALWRAPPER).should('contain.text', '2510');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       2
@@ -134,15 +134,15 @@ describe('burger constructor functional tests', () => {
   });
 
   it('can reorder ingredients in the constructor', () => {
-    cy.get('@ingredient').eq(0).as('first');
-    cy.get('@ingredient').eq(2).as('third');
-    cy.get('@ingredient').eq(8).as('ninth');
-    cy.get('@first').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@third').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
-    cy.get('@ninth').trigger('dragstart');
-    cy.get('@constructor').trigger('drop');
+    cy.get(SELECTORS.INGREDIENT).eq(0).as('first');
+    cy.get(SELECTORS.INGREDIENT).eq(2).as('third');
+    cy.get(SELECTORS.INGREDIENT).eq(8).as('ninth');
+    cy.get(SELECTORS.FIRST).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.THIRD).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.NINTH).trigger('dragstart');
+    cy.get(SELECTORS.CONSTRUCTOR).trigger('drop');
     cy.get(SELECTORS.BURGER_CONSTRUCTOR_INGREDIENT).should(
       'have.length',
       4
--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -1,3 +1,14 @@
 module.exports.SELECTORS = {
+  FIRST: '@first',
+  SECOND: '@second',
+  THIRD: '@third',
+  NINTH: '@ninth',
+  TOTALWRAPPER: '@totalWrapper',
+  INGREDIENT: '@ingredient',
+  CONSTRUCTOR: '@constructor',
   BURGER_CONSTRUCTOR_INGREDIENT: '[data-test-id="burger-constructor-ingredient"]',
+};
+
+module.exports.TESTKEY = {
+  DATA_TEST_ID: 'data-test-id'
 };
